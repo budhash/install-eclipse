de6faa5a505fe0d4c26d9f9f12f5ff78a280ce23
budhash <budhash@gmail.com>
2016-07-20 02:20:54 -0700

    minor test fix

--- install_test.sh
+++ install_test.sh
@@ -1,3 +1,5 @@
 #simple test
 ./install-eclipse -f -c https://raw.githubusercontent.com/budhash/install-eclipse/master/profiles/git-java-mvn.cfg ./eclipse
-exit $?
+status=$?
+cat install-eclipse.log
+exit $status
